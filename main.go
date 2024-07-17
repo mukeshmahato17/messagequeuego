40d37c56ecd3236eb5155bb042c00764b4d132b3
mukeshmahato17 <mukeshmahato1089@gmail.com>
2024-07-17 20:17:20 +0545

    main.go

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+func main() {
+	cfg := &Config{
+		ListenAddr: ":3000",
+		Store:      NewMemoryStore(),
+	}
+	s, err := NewServer(cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+	s.Store.Push([]byte("foobarbaz"))
+	data, err := s.Store.Fetch(1)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(data))
+}
